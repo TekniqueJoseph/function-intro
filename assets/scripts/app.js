4d9990872f6a1b128d4e5331ebbc3155d8d18d7b
TekniqueJoseph <joejacksun@gmail.com>
2023-04-14 09:50:59 -0400

    addwd arrow functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,21 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    return result
+// function add(num1, num2) {
+//     const result = num1 + num2;
+//     return result
+// }
+
+const theResult = (n1, n2) => {
+    const result = n1 + n2;
+    return `The result is: ${result}`
 }
-
-currentResult = add(1,2);
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-outputResult(currentResult, calculationDescription);
+console.log(theResult(2,3));
 
 
 
+
+// currentResult = add(1,2);
+// let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+// outputResult(currentResult, calculationDescription);
