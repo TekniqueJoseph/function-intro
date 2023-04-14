0a9abceb287a48a3676e2a5c2976995e2f6b48e9
TekniqueJoseph <joejacksun@gmail.com>
2023-04-13 20:27:30 -0400

    added assignment code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,19 +1,16 @@
-// const defaultResult = 0;
-// let currentResult = defaultResult;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-// function add(num1, num2) {
-//     const result = num1 + num2;
-//     return result
-// }
-
-// currentResult = add(1,2);
-
-// let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-
-// outputResult(currentResult, calculationDescription);
-
-function stringify(number) {
-    return 'Result:' + number
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result
 }
 
-console.log(stringify(10))
+currentResult = add(1,2);
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
+
+
+
