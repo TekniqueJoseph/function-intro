e72aa643f74291240b7fa192f617322a171689bd
TekniqueJoseph <joejacksun@gmail.com>
2023-04-14 11:15:11 -0400

    addwd return with template strings

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,6 +15,3 @@ console.log(theResult(2,3));
 // currentResult = add(1,2);
 // let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 // outputResult(currentResult, calculationDescription);
-
-
-
