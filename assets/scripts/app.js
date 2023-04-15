2ebb1ffe5cfa5b90d70fef407029dca56eca3640
TekniqueJoseph <joejacksun@gmail.com>
2023-04-14 21:06:49 -0400

    added lecture exercise completed

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,28 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
-}
+// function add() {
+//     currentResult = currentResult + userInput.value;
+//     outputResult(currentResult, '');
+// }
 
-addBtn.addEventListener('click', add);
+// addBtn.addEventListener('click', add);
+
+function double(num) {
+    return num * 2;
+}
+// console.log(double(2))
+
+function transform(num, double){
+    return (double(num))
+} 
+
+console.log(transform(10,double))
+
+// function transform(num, double){
+//     return (double(num))
+// } 
+
+// console.log(transform(10,double))
+
 
