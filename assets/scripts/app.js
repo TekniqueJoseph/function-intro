949ab75ad9e40cf824308282967fa1e2272fadae
TekniqueJoseph <joejacksun@gmail.com>
2023-04-15 15:24:49 -0400

    added functions to make calc function work

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,17 +5,44 @@ function getUserNumberInput() {
     return parseInt(usrInput.value)
 }
 
-function add() {
-    const enteredNumber = getUserNumberInput() 
-    const calcDesc = `${currentResult} + ${enteredNumber}`  
-    currentResult = currentResult + parseInt(enteredNumber);
+function createAndWriteLog(operator, resultBeforeCalc, calcNum) {
+    const calcDesc = `${resultBeforeCalc} ${operator}  ${calcNum}`
     outputResult(currentResult, calcDesc);
 }
 
+function add() {
+    const enteredNumber = getUserNumberInput() 
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteLog('+', initialResult, enteredNumber)
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput() 
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteLog('-', initialResult, enteredNumber)
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput() 
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteLog('*', initialResult, enteredNumber)
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput() 
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteLog('/', initialResult, enteredNumber)
+
+}
+
 addBtn.addEventListener('click', add);
-
-
-
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 
 
