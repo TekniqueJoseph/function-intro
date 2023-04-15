51a49f14673122fddca52706cb1e4186141c9b0d
TekniqueJoseph <joejacksun@gmail.com>
2023-04-14 20:11:58 -0400

    added lecture code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,10 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-// function add(num1, num2) {
-//     const result = num1 + num2;
-//     return result
-// }
-
-const theResult = (n1, n2) => {
-    const result = n1 + n2;
-    return `The Result Is: ${result}`
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
 }
-console.log(theResult(2,3));
 
-// currentResult = add(1,2);
-// let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
-// outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
+
