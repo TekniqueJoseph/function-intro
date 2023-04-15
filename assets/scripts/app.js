e92381ed738795a4af9c69bb30fb79e2fe01d6d4
TekniqueJoseph <joejacksun@gmail.com>
2023-04-15 14:01:40 -0400

    added function to get uder input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function getUserNumberInput() {
+    return parseInt(usrInput.value)
+}
+
 function add() {
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult,'');
+    const enteredNumber = getUserNumberInput() 
+    const calcDesc = `${currentResult} + ${enteredNumber}`  
+    currentResult = currentResult + parseInt(enteredNumber);
+    outputResult(currentResult, calcDesc);
 }
 
 addBtn.addEventListener('click', add);
