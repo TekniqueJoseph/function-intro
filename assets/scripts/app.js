3fee76a65a0efb176dacb2bda01ab08bf4a3b9d3
TekniqueJoseph <joejacksun@gmail.com>
2023-04-15 13:13:58 -0400

    added lecture code with toString method

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,24 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-// function add() {
-//     currentResult = currentResult + userInput.value;
-//     outputResult(currentResult,'');
-// }
+function add() {
+    currentResult = currentResult + parseInt(userInput.value);
+    outputResult(currentResult,'');
+}
 
-// addBtn.addEventListener('click', add);
+addBtn.addEventListener('click', add);
 
 
-// function double(num) {
-//     return num * 2;
-// }
-// console.log(double(2))
-
-// function transform(num, double){
-//     return (double(num))
-// } 
-
-// console.log(transform(10,double))
 
 
 
